f3a4cc8eed45719b81eb7c2f76baefcc35100f7d
Chacha <neerajdevi@atomicmail.io>
2025-11-26 02:21:34 +0000

    vars.py edited online with Bitbucket

--- vars.py
+++ vars.py
@@ -6,7 +6,7 @@ API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-CREDIT = os.environ.get("CREDIT", "🖤⃝𝐒𝐨𝐧𝐮")
+CREDIT = os.environ.get("CREDIT", "@Contact_AdminS2bot")
 # MongoDB Configuration
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Sonu")
 DATABASE_URL = os.environ.get("DATABASE_URL", "")  # Add your own atlas db
